94b7d0ad09ddfa725edf47f0a36ed6fa7c4ac223
Lal Krishna <134937814+STARK-404@users.noreply.github.com>
2024-01-17 22:58:01 +0530

    Create 7.cpp

---
+++ 7.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+int weeknumber;
+cout<<"Enter week number(1-7): ";
+cin>>weeknumber;
+switch(weeknumber)
+{
+case 1: cout<<"Monday";
+break;
+case 2: cout<<"Tuesday";
+break;
+case 3: cout<<"Wednesday";
+break;
+case 4: cout<<"Thursday";
+break;
+case 5: cout<<"Friday";
+break;
+case 6: cout<<"Saturday";
+break;
+case 7: cout<<"Sunday";
+break;
+default: cout<<"Invalid input! Please enter week no. between 1-7.";
+}
+return 0;
+}
