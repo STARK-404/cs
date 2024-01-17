2654f07b8c9c36b6c4bf2ef00ff25b7e50ba5f67
Lal Krishna <134937814+STARK-404@users.noreply.github.com>
2024-01-17 22:42:01 +0530

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+int n,sum=0;
+
+cout<<"\n enter a positive integer\n";
+
+cin>>n;
+
+for(int i=1; i<=n; ++i)
+
+sum+= i*i;
+
+{
+
+}
+
+cout<<" sum of squares is "<<sum;
+
+return 0;
+
+}
